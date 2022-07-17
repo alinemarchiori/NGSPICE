8dfccd74b10591aa2f56b3d65c92e8cbd86c3d15
Aline Marchiori <alineeuu@outlook.com>
2022-07-17 15:10:07 -0300

    pwls prontas e criacao do arquivo

--- gerador_de_circuito.py
+++ gerador_de_circuito.py
@@ -7,6 +7,14 @@ def entrada_de_dados():
     nome_circuito = input("Digite o nome do circuito: ")
     ini_string = "e0"
 
+    # dados iniciais
+    lista_de_linhas_arquivo_final.append(nome_circuito)
+    lista_de_linhas_arquivo_final.append('\n*PARAMETROS')
+    lista_de_linhas_arquivo_final.append('.include 32nm_HP.pm')
+    lista_de_linhas_arquivo_final.append('\n*DECLARANDO FONTES DE TENSAO')
+    lista_de_linhas_arquivo_final.append('Vvdd vdd gnd 1')
+    lista_de_linhas_arquivo_final.append('\n*DECLARACAO DAS FONTES')
+
     return nome_circuito, ini_string
 
 
@@ -19,15 +27,10 @@ def gera_binario(dado_de_entrada):
 
 
 def cria_arquivo_spice(nome_circuito):
-    global lista_de_linhas_arquivo_final
     arquivo = open(f'{nome_circuito}.txt', 'w+')
-    
-    # dados iniciais
-    lista_de_linhas_arquivo_final.append(nome_circuito)
-    lista_de_linhas_arquivo_final.append('.include 32nm_HP.pm')
-    lista_de_linhas_arquivo_final.append('Vvdd vdd gnd 1')
 
     return arquivo
+
 
 # Escreve uma única vez no arquivo (chamar apenas no final)
 def escreve_no_arquivo_spice(arquivo, dados_para_o_arquivo):
@@ -51,7 +54,7 @@ def gera_tabela_verdade(numero_de_fontes, saida_tabela):
     for numero in range(numero_de_fontes):
         quantidade_de_zeros //= 2
         
-        while len(coluna) < 8:
+        while len(coluna) < len(saida_tabela):
             for zero in range(quantidade_de_zeros):
                 coluna.append(0)
 
@@ -71,18 +74,18 @@ def gera_tabela_verdade(numero_de_fontes, saida_tabela):
 
 def pwl(tabela_verdade):
     pwls = []
-    numero_de_fontes = len(tabela_verdade) - 1
-    numero_de_saidas = len(tabela_verdade[-1])  
-    nome = 0
-    for elemento in tabela_verdade:
-        intervalo = ''
-        for sinal in range(len(elemento) - 1):
-            if elemento[sinal] != elemento[sinal + 1]:
-                intervalo += f'{(sinal-1) * 2}ns {elemento[sinal]} {((sinal-1) * 2) + 0.1}ns {elemento[sinal + 1]} '
+    for elemento in range(len(tabela_verdade) - 1):
+        intervalo = f'Vfonte{elemento} fonte{elemento} gnd PWL (0ns 0 '
+        for sinal in range(len(tabela_verdade[elemento]) - 1):
 
-        string_fonte = f'V{nome} {nome} gnd PWL (0ns 0 {intervalo})'
-        pwls.append(string_fonte)
-        nome += 1
+            if tabela_verdade[elemento][sinal] != tabela_verdade[elemento][sinal + 1] and sinal == len(tabela_verdade[elemento]) - 2:
+                intervalo += f'{(sinal+1)*2}ns {tabela_verdade[elemento][sinal]} {((sinal+1)*2) + 0.1}ns {tabela_verdade[elemento][sinal + 1]}'
+
+            elif tabela_verdade[elemento][sinal] != tabela_verdade[elemento][sinal + 1]:
+                intervalo += f'{(sinal+1)*2}ns {tabela_verdade[elemento][sinal]} {((sinal+1)*2) + 0.1}ns {tabela_verdade[elemento][sinal + 1]} '
+
+        intervalo += ')'
+        pwls.append(intervalo)
 
     return pwls
 
@@ -91,15 +94,29 @@ def atrasos():
 
 
 def main():
+    global lista_de_linhas_arquivo_final
+
     nome, string_hexadecimal = entrada_de_dados()
+
     string_binario = gera_binario(string_hexadecimal)
-    print(string_binario)
+    #print(string_binario)
+
     log_fontes = define_numero_de_fontes(len(string_binario))
-    print(log_fontes)
+    #print(log_fontes)
+
     tabela_verdade = gera_tabela_verdade(log_fontes, string_binario)
-    print(tabela_verdade)
+    #print(tabela_verdade)
+
     lista_pwls = pwl(tabela_verdade)
-    for i in lista_pwls:
-        print(i)
+    
+    lista_de_linhas_arquivo_final += lista_pwls
+
+    lista_de_linhas_arquivo_final.append('\n*DECLARAR O CIRCUITO\n\n*...\n')
+    lista_de_linhas_arquivo_final.append('*SIMULACAO TRANSIENTE DE 32ns COM PASSO DE 0.1ns')
+    lista_de_linhas_arquivo_final.append('.tran 0.1ns 32ns\n')
+
+    arquivo = cria_arquivo_spice(nome)
+
+    escreve_no_arquivo_spice(arquivo, lista_de_linhas_arquivo_final)
 
 main()
